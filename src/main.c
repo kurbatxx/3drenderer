426e49f52729af41f8fd08ab2287730432ab9e36
Kurbat <kurbatxx@gmail.com>
2024-03-20 17:53:31 +0500

    Creating An Sdl Window

--- src/main.c
+++ src/main.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
 
-int main(void)
-{
-    printf("hello, world!\n");
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+bool initialize_window(void) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        fprintf(stderr, "Error initializing SDL.\n");
+        return false;
+    };
+
+    // Create a SDL Window
+    window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_BORDERLESS
+    );
+
+    if (!window) {
+        fprintf(stderr, "Error creating SDL window.\n");
+        return false;
+    }
+
+    // Create a SDL renderer
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer) {
+        fprintf(stderr, "Error creating SDL rendere.\n");
+        return false;
+    }
+
+    return true;
+}
+
+int main(void) {
+    // create a SDL window
+    is_running = initialize_window();
     return 0;
 }
