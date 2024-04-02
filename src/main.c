794ca501bdcbafb5f53b7a07a724d66c7329bcd5
Kurbat <kurbatxx@gmail.com>
2024-04-02 14:59:25 +0500

    Array Of Points

--- src/main.c
+++ src/main.c
@@ -5,6 +5,10 @@
 #include "display.h"
 #include "vector.h"
 
+// Declare an array of vectors/points
+#define N_POINTS (9 * 9 * 9)
+vec3_t cube_points[N_POINTS]; // 9*9*9 cube
+
 bool is_running = false;
 
 void setup(void) {
@@ -19,6 +23,19 @@ void setup(void) {
         window_width,
         window_height
     );
+
+
+    // Start loading array of vectors
+    // From -1 to 1 (in this 9x9x9 cube)
+    int point_count = 0;
+    for (float x = -1; x <= 1; x += 0.25) {
+        for (float y = -1; y <= 1; y += 0.25) {
+            for (float z = -1; z <= 1; z += 0.25) {
+                vec3_t new_point = { .x = x, .y = y, .z = z };
+                cube_points[point_count++] = new_point;
+            }
+        }
+    }
 
 }
 
@@ -62,8 +79,6 @@ int main(void) {
     is_running = initialize_window();
 
     setup();
-
-    vec3_t myvector = {2.0, 3.0, -4.0};
 
     while (is_running) {
         process_input();
