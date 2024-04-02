363fba157b61c72c3a31b15cd9e358e49f917e70
Kurbat <kurbatxx@gmail.com>
2024-04-02 14:23:32 +0500

    Declaring A Vector Type

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 #include "display.h"
+#include "vector.h"
 
 bool is_running = false;
 
@@ -61,6 +62,8 @@ int main(void) {
     is_running = initialize_window();
 
     setup();
+
+    vec3_t myvector = {2.0, 3.0, -4.0};
 
     while (is_running) {
         process_input();
